047e23367a3751f96447b414ac34e2b8cedc2f3a
sooin923 <sooin051215@hanyang.ac.kr>
2024-10-08 23:19:52 +0900

    class_1021

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todolist_eos/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,71 +16,71 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Pretendard',
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFFA4C639).withOpacity(0.1),
-        title: Text('EOS ToDoList'),
-        leading: Image.asset('assets/images/eos_logo.png'),
-      ),
-
-
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(25),
-            height: 200,
-            child:Row(
-              children: [
-                Container(
-                  width: 140,
-                  height: 140,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(width:10, color: Colors.grey),
-                    borderRadius: BorderRadius.circular(100),
-                  ),
-                  child:Center(
-                    child: Image.asset('assets/images/eos_logo.png')
-                  ),
-                ),
-                SizedBox(
-                  width: 35,
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '조수인',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Text('hello!')
-                    ]),
-                )
-              ])),
-          Container(
-            height: 500,
-            color: Color(0xFFA4C639).withOpacity(0.1),
-          ),
-
-        ],
-      ));
-  }
-}
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Color(0xFFA4C639).withOpacity(0.1),
+//         title: Text('EOS ToDoList'),
+//         leading: Image.asset('assets/images/eos_logo.png'),
+//       ),
+//
+//
+//       body: Column(
+//         children: [
+//           Container(
+//             padding: EdgeInsets.all(25),
+//             height: 200,
+//             child:Row(
+//               children: [
+//                 Container(
+//                   width: 140,
+//                   height: 140,
+//                   decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     border: Border.all(width:10, color: Colors.grey),
+//                     borderRadius: BorderRadius.circular(100),
+//                   ),
+//                   child:Center(
+//                     child: Image.asset('assets/images/eos_logo.png')
+//                   ),
+//                 ),
+//                 SizedBox(
+//                   width: 35,
+//                 ),
+//                 Expanded(
+//                   child: Column(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       Text(
+//                         '조수인',
+//                         style: TextStyle(
+//                           fontSize: 20,
+//                           fontWeight: FontWeight.bold,
+//                         ),
+//                       ),
+//                       SizedBox(
+//                         height: 15,
+//                       ),
+//                       Text('hello!')
+//                     ]),
+//                 )
+//               ])),
+//           Container(
+//             height: 500,
+//             color: Color(0xFFA4C639).withOpacity(0.1),
+//           ),
+//
+//         ],
+//       ));
+//   }
+// }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFA4C639).withOpacity(0.1),
+        title: Text('EOS ToDoList'),
+        leading: Image.asset('assets/images/eos_logo.png'),
+      ),
+
+
+      body: Column(
+        children: [
+          Container(
+              padding: EdgeInsets.all(25),
+              height: 200,
+              child:Row(
+                  children: [
+                    Container(
+                      width: 140,
+                      height: 140,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(width:10, color: Colors.grey),
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child:Center(
+                          child: Image.asset('assets/images/eos_logo.png')
+                      ),
+                    ),
+                    SizedBox(
+                      width: 35,
+                    ),
+                    Expanded(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '조수인',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Text('hello!')
+                          ]),
+                    )
+                  ])),
+          Stack(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 500,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFA4C639).withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 20,
+                left: (MediaQuery.of(context).size.width - 150)/2,
+                child: Container(
+                  width: 150,
+                  height: 38,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFA4C639).withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+
+                  child: Center(
+                    child: Text(
+                      "To do list",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 23,
+                        fontWeight: FontWeight.w600
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ));
+}
+}
+
